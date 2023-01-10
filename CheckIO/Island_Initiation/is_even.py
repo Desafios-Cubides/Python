92884ee7209a7c2fce54f008d23280b337820b30
CZzynox <zzynox2017@gmail.com>
2023-01-10 18:36:59 -0500

    resuleto

---
+++ CheckIO/Island_Initiation/is_even.py
@@ -0,0 +1,12 @@
+'''Link (https://py.checkio.org/en/mission/is-even/)
+'''
+
+def is_even(num):
+
+    if (num % 2 == 0):
+        return True
+    else:
+        return False
+
+
+print(is_even(4))
